4d35526893c70946809bf476765707a50c5cf37f
Matthias Meusburger <matthias.meusburger@biblibre.com>
2020-09-14 16:35:51 +0200

    DIBS plugin: use authkey parameter to validate request in callback.pl

--- Koha/Plugin/Com/BibLibre/DIBSPayments/callback.pl
+++ Koha/Plugin/Com/BibLibre/DIBSPayments/callback.pl
@@ -30,21 +30,59 @@ use Koha::Plugin::Com::BibLibre::DIBSPayments;
 
 use XML::LibXML;
 use Digest::MD5 qw(md5_hex);
+use Locale::Currency;
+use Locale::Currency::Format;
 
 my $paymentHandler = Koha::Plugin::Com::BibLibre::DIBSPayments->new;
 my $input = new CGI;
 my $statuscode = $input->param('statuscode');
 
 if ($statuscode and $statuscode == 2) {
-    my $totalamount = $input->param('totalamount');
-    my $transaction_id = $input->param('orderid');
 
+    my $koha_transaction_id = $input->param('orderid');
+    warn "orderid missing" and return unless ($koha_transaction_id);
+
+    my $dibs_transaction_id = $input->param('transact');
+    warn "transact missing" and return unless ($dibs_transaction_id);
+
+    my $authkey = $input->param('authkey');
+    warn "authkey missing" and return unless ($authkey);
+
+    # We are getting the amount from the database and not from the parameters,
+    # because this page says it can be returned:
+    # https://tech.dibspayment.com/D2/Hosted/Output_parameters/Return_parameters
+    # this page doesn't mention it:
+    # https://tech.dibspayment.com/D2/Hosted/Output_parameters/Return_parameter_configuration
+    # And even though it is selectable in the admin configuration (Integration -> return values)
+    # it is never returned
     my $table = $paymentHandler->get_qualified_table_name('dibs_transactions');
     my $dbh   = C4::Context->dbh;
     my $sth   = $dbh->prepare(
         "SELECT borrowernumber, accountlines_ids, amount FROM $table WHERE transaction_id = ?");
-    $sth->execute($transaction_id);
+    $sth->execute($koha_transaction_id);
     my ($borrowernumber, $accountlines_string, $amount) = $sth->fetchrow_array();
+
+    my $active_currency = Koha::Acquisition::Currencies->get_active;
+    my $local_currency;
+    if ($active_currency) {
+        $local_currency = $active_currency->isocode;
+        $local_currency = $active_currency->currency unless defined $local_currency;
+    } else {
+        $local_currency = 'EUR';
+    }
+    my $currency = code2currency($local_currency, LOCALE_CURR_ALPHA);
+    my $currency_number = currency2code($currency, LOCALE_CURR_NUMERIC);
+    my $decimals = decimal_precision($local_currency);
+
+    # DIBS require "The smallest unit of an amount in the selected currency, following the ISO4217 standard." 
+    $md5amount = $amount * 10**$decimals;
+
+    my $md5string = "transact=$dibs_transaction_id&amount=$md5amount&currency=$currency_number";
+    my $md51 = md5_hex($paymentHandler->retrieve_data('MD5k1') . $md5string);
+    my $md5checksum = md5_hex($paymentHandler->retrieve_data('MD5k2') . $md51);
+
+    warn "wrong authkey" and return unless ($authkey == $md5checksum);
+
     my @accountline_ids = split(' ', $accountlines_string);
     my $borrower = Koha::Patrons->find($borrowernumber);
     my $lines = Koha::Account::Lines->search(
@@ -63,7 +101,7 @@ if ($statuscode and $statuscode == 2) {
     my $dbh   = C4::Context->dbh;
     my $sth   = $dbh->prepare(
         "UPDATE $table SET accountline_id = ? WHERE transaction_id = ?");
-    $sth->execute( $accountline_id, $transaction_id );
+    $sth->execute( $accountline_id, $koha_transaction_id );
     
 	# Renew any items as required
     for my $line ( @{$lines} ) {
